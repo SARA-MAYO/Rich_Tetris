fa65b39f5dca6f9cde5ddc23806c6be83d535498
Jin Wuk <jdk010705@gmail.com>
2025-11-19 15:53:14 +0900

    점수계산 메서드 구현

--- state/score.py
+++ state/score.py
@@ -1,1 +1,15 @@
-print("good")
+from src.config.settings import SCORE_MAP
+
+class ScoreManager:
+    def __init__(self):
+        self.score = 0
+        self.level = 1
+        self.total_lines = 0
+
+    def add_lines(self, lines):
+        if lines > 0:
+            self.total_lines += lines
+            # 점수 계산
+            self.score += SCORE_MAP.get(lines, 0) * self.level
+            # 레벨업 (10줄마다)
+            self.level = 1 + (self.total_lines // 10)
