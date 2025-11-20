ceb9b58b33479c6c4473b14305bd7a90260e7f4e
SARA-MAYO <seolha.lee423@gmail.com>
2025-11-21 03:02:42 +0900

    feat: O 블록 예외 처리

--- logic/rotator.py
+++ logic/rotator.py
@@ -1,1 +1,11 @@
-print(1+1)
+"""
+테트로미노의 현재 좌표를 기반으로 시계방향 90도 회전된 새 좌표를 계산해서 반환
+- 회전 기능 구현을 위해 회전 계산만 담당하는 순수 로직 제공
+"""
+
+def calculate_rotation(shape_name, current_coords):
+
+    # 예외 처리 '0' 블록은 회전안 함(정사각형 형태)
+    if shape_name == "O":
+        return current_coords  
+
